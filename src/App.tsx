0d65fc321f38b510abb1303635703053a86bc654
Br3zzly <loeschdu@gmail.com>
2025-10-09 11:59:08 +0200

    Footer changes

--- src/App.tsx
+++ src/App.tsx
@@ -193,11 +193,11 @@ export default function App() {
           )}
         </div>
         <footer className="w-full mt-8 py-4  text-white text-center text-sm  ">
-          Made by <a href="https://github.com/br3zzly" className="underline">Br3zzly</a><br />
-          With help from <a href="https://github.com/theezeb" className="underline">theezeb</a>, <a href="https://github.com/btastic" className="underline">btastic</a>, <a href="https://github.com/TheSaneWriter" className="underline">TheSaneWriter</a>, <a href="https://github.com/saiki-k" className="underline">saiki-k</a><br />
-          <a href="https://github.com/Br3zzly/hk-silksong-savegame-analyzer" className="underline">GitHub Repo</a><br />
-          <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3571462700" className="underline">Steam Guide</a><br />
-          <a href="https://www.buymeacoffee.com/Br3zzly" className="underline">Buy me a coffee</a>
+          Made by <a href="https://github.com/br3zzly" target="_blank" className="underline">Br3zzly</a><br />
+          With help from <a href="https://github.com/theezeb" target="_blank" className="underline">theezeb</a>, <a href="https://github.com/btastic" target="_blank" className="underline">btastic</a>, <a href="https://github.com/TheSaneWriter" target="_blank" className="underline">TheSaneWriter</a>, <a href="https://github.com/saiki-k" target="_blank" className="underline">saiki-k</a><br />
+          <a href="https://github.com/Br3zzly/hk-silksong-savegame-analyzer" target="_blank"className="underline">GitHub Repo</a><br />
+          <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3571462700" target="_blank" className="underline">Steam Guide</a><br />
+          <a href="https://www.buymeacoffee.com/Br3zzly" target="_blank" className="underline">Buy me a coffee</a>
         </footer>
       </div>
     </div>
