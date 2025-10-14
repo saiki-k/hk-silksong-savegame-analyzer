da7d67ed0fe1748c483c54519403622f70c0df2d
Sai <saikishore.bits@gmail.com>
2025-10-14 21:00:15 +0530

    Update mossberries description + Remove the TODO comment post-verification

--- src/parsers/categories/mossberries.ts
+++ src/parsers/categories/mossberries.ts
@@ -2,13 +2,12 @@ import type { NormalisedTrackableCategory } from '../types';
 
 export const mossberries: NormalisedTrackableCategory = {
   name: 'Mossberries',
-  description: 'Poisonous fruits, initially gathered as a part of a wish, later used to trade for Rosaries and other rewards.',
+  description: 'Sweet berries with poisonous seeds, that grow in moist environments. Initially gathered as a part of a wish, later used to trade for Rosaries and other rewards.',
   sections: [
     {
       name: 'Main',
       description: '',
       items: [
-        // TODO: Verify that the internalIds for Mossberry #1 and Mossberry #2 are not swapped
         { name: 'Mossberry #1', whichAct: 1, completionPercent: 0, prereqs: [], location: 'Moss Grotto', parsingInfo: { type: 'sceneData', internalId: ['Tut_02', 'moss_berry_fruit'] }, mapLink: 'https://mapgenie.io/hollow-knight-silksong/maps/pharloom?locationIds=476900' },
         { name: 'Mossberry #2', whichAct: 1, completionPercent: 0, prereqs: [], location: 'Moss Grotto: Pogo off of a nearby Mossmir to reach it.', parsingInfo: { type: 'sceneData', internalId: ['Tut_01b', 'moss_berry_fruit'] }, mapLink: 'https://mapgenie.io/hollow-knight-silksong/maps/pharloom?locationIds=476909' },
         { name: 'Mossberry #3', whichAct: 1, completionPercent: 0, prereqs: [], location: 'Moss Grotto (Bone Bottom): Held by a flying enemy high above the settlement. Moves into a room east of here in ACT 3.', parsingInfo: { type: 'flag', internalId: 'bonetownAspidBerryCollected' }, mapLink: 'https://mapgenie.io/hollow-knight-silksong/maps/pharloom?locationIds=478141' },
