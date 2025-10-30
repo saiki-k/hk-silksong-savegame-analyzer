983e956717f97b36111600a23d97e6077a19c6a1
Br3zzly <loeschdu@gmail.com>
2025-10-30 15:56:32 +0100

    Fixed Devices tracking

--- src/dictionary/categories/devices.ts
+++ src/dictionary/categories/devices.ts
@@ -3,7 +3,7 @@ import type { TrackableCategory } from "../types";
 export const devices: TrackableCategory = {
   name: "Devices",
   description:
-    "Devices that can be assembled in your Bellhome in Bellhart that track items / progress.",
+    "Devices that track items / progress. They only get tracked once you install them in your Bellhome in Bellhart.",
   sections: [
     {
       items: [
@@ -11,14 +11,14 @@ export const devices: TrackableCategory = {
           name: "Materium",
           whichAct: 2,
           completionDetails: 'Memorium: Can be found in the upper part of the Memorium. The item must be constructed in the Bellhome in Bellhart before it can be used.',
-          parsingInfo: [{ type: "sceneData", internalId: ["Arborium_07", "Collectable Item Pickup Materium"] },{ type: "flag", internalId: "ConstructedMaterium" }],
+          parsingInfo: { type: "flag", internalId: "ConstructedMaterium" },
           mapLink: "https://mapgenie.io/hollow-knight-silksong/maps/pharloom?locationIds=479119",
         },
         {
           name: "Farsight",
           whichAct: 3,
           completionDetails: 'Abyss: Found in the upper east passage of Weavenest Absolom. The item must be constructed in the Bellhome in Bellhart before it can be used.',
-          parsingInfo: [{ type: "sceneData", internalId: ["Abyss_08", "Collectable Item Pickup"] },{ type: "flag", internalId: "ConstructedFarsight" }],
+          parsingInfo: { type: "flag", internalId: "ConstructedFarsight" },
           mapLink: "https://mapgenie.io/hollow-knight-silksong/maps/pharloom?locationIds=479289",
         },
       ],
